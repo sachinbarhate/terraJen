c045e25696774c631ab3cbf777000bf61015ec4b
nwaykole <nutanwaykole25@gmail.com>
2021-03-05 17:11:10 +0530

    hj

--- provider.tf
+++ provider.tf
@@ -3,11 +3,13 @@ provider "aws" {
   secret_key = "f0hqtAMudT02Qq1aI4gh4UhcAawd+1k5z9xQtHKL"
   region     = "us-east-2"
 }
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraformstat"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-2"
-#     #dynamodb_table = "javahome-tf"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket     = "terraformstat"
+    key        = "terraform.tfstate"
+    region     = "us-east-2"
+    access_key = "AKIARKHDULTZRW4U57M4"
+    secret_key = "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"
+    #dynamodb_table = "javahome-tf"
+  }
+}
